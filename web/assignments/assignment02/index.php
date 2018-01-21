427334c73a91fd343c27c216a7a1bcf7f35fe80e
Gregory Harston <har11057@byui.edu>
2018-01-20 20:22:58 -0700

    added files for home page

---
+++ web/assignments/assignment02/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <title>Gregory's Home Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+	
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">Gregory's Home Page</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="../index.php">Assignments</a></li>
+			</ul>
+		</div>
+	</nav>
+	
+	<div class="container-fluid">
+		
+	</div>
+
+	<div class="jumbotron">
+		<div class="container text-center">
+			<p>Boating with my Girls</p>
+			<img src="../images/family.jpg" class="img-thumbnail" alt="Family Photo">	
+		</div>
+	</div>
+  
+	<div class="container-fluid bg-3 text-center">    
+		<h3>Some of my Work</h3><br>
+		<div class="row">
+			<div class="col-sm-3">
+				<p>Some text..</p>
+				<img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+			</div>
+			<div class="col-sm-3"> 
+			<p>Some text..</p>
+				<img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+			</div>
+			<div class="col-sm-3"> 
+				<p>Some text..</p>
+				<img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+			</div>
+			<div class="col-sm-3">
+				<p>Some text..</p>
+				<img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+			</div>
+		</div>
+	</div><br>
+
+	<div class="container-fluid bg-3 text-center">    
+		<div class="row">
+			<div class="col-sm-3">
+				<p>Some text..</p>
+				<img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+			</div>
+			<div class="col-sm-3"> 
+				<p>Some text..</p>
+				<img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+			</div>
+			<div class="col-sm-3"> 
+				<p>Some text..</p>
+				<img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+			</div>
+			<div class="col-sm-3">
+				<p>Some text..</p>
+				<img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+			</div>
+		</div>
+	</div><br><br>
+
+	<footer class="container-fluid text-center">
+		<p>Footer Text</p>
+	</footer>
+
+</body>
+</html>
---
+++ web/assignments/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <title>Gregory's Home Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+	
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="assignment02/index.php">Gregory's Home Page</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href="assignment02/index.php">Home</a></li>
+				<li class="active"><a href="#">Assignments</a></li>
+			</ul>
+		</div>
+	</nav>
+	<h1> COMMING SOON </h1>
+</body>
+</html>
