1e3a1dd1f9711ce4ded3acfcc5b4f9d2a95340c0
Gregory Harston <har11057@byui.edu>
2018-01-29 20:38:45 -0700

    assignment 3 added

--- web/assignments/assignment03/browse.php
+++ web/assignments/assignment03/browse.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+$images = array('images/162_wood_floor_cleaner.jpg','images/resolve.jpg');
+$product_array = array('Wood Floor Cleaner', 'Resolve');
+$price_array = array('15.99', '10.99');
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,40 +20,37 @@ require 'assign03header.php';
 			<p>Mission, Vision & Values</p>
 		</div>
 	</div>
-	
-	<div class="container">    
-		<div class="row">
-			
+
+<div class="container">
+	<div class="row">
+<?php
+for($i= 0; $i < count($images); $i++){
+	$_SESSION["$i"] = $images[$i];
+
+	echo '		<form method="post" action="addItems.php">';
+	echo '		<div class="col-sm-4" style="width:auto">'."\n";
+	echo '			<div class="panel panel-primary">'."\n";
+	echo '				<div class="panel-heading">'.$product_array[$i].'</div>'."\n";
+	echo '				<div class="panel-body"><img class="thumb-nail" src="'.$images[$i].'"/></div>'."\n";
+	echo '				<div class="panel-footer"><input type="hidden" name="product" value="'. $product_array[$i] .'"/><input class="number_input" type="number" size="2" name="count"/><input type="submit" value="Add to Cart"/></div>'."\n";
+	echo '			</div>'."\n";
+	echo '		</div>'."\n";
+	echo ' 		</form>';
+}
+?>
+	</div>
+</div>
+
+<!--	<div class="container">    
+		<div class="row">			
 			<div class="col-sm-4">
-			<input type="checkbox">
 				<div class="panel panel-primary">
 					<div class="panel-heading">BLACK FRIDAY DEAL</div>
-					<div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
-					<div class="panel-footer">Buy 50 mobiles and get a gift card</div>
-				</div>
-			</input>
-			</div>
-			
-			<div class="col-sm-4"> 
-			<input type="checkbox">
-				<div class="panel panel-danger">
-					<div class="panel-heading">BLACK FRIDAY DEAL</div>
-					<div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
-					<div class="panel-footer">Buy 50 mobiles and get a gift card</div>
-				</div>
-			</input>
-			</div>
-			
-			<input type="checkbox">
-			<div class="col-sm-4"> 
-				<div class="panel panel-success">
-					<div class="panel-heading">BLACK FRIDAY DEAL</div>
-					<div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
+					<div class="panel-body"><img name="image" src="images/162_wood_floor_cleaner.jpg" class="img-responsive" style="width:100%" alt="Image"></div>				
 					<div class="panel-footer">Buy 50 mobiles and get a gift card</div>
 				</div>
 			</div>
-			</input>
 		</div>
-	</div><br>
+	</div><br>-->
 </body>
 </html>
--- web/assignments/assignment03/viewCart.php
+++ web/assignments/assignment03/viewCart.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$items = $_SESSION['products'];
 ?>
 
 <!DOCTYPE html>
@@ -10,11 +11,19 @@ require 'assign03header.php';
 ?>
 
 <body class="light-background">
-<?php
-$_SESSION["name"] = "Greg";
-echo 'Session variables are set';
-?>
 
+<?php
+
+echo '<div class="container container-fluid">';
+foreach($items as $item=>$count){
+	echo '<div class="panel panel-primary"> ' . $count[key($count)] . '     ' . key($count) . '</div><br/>';
+}
+echo '</div>';
+?>
+<a href="checkout.php"><button type="submit">Checkout</button></a>
 </body>
 
 </html>
+
+
+
