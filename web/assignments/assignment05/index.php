311885c6f0e876a2a1868ceb7442f4acbae11c6a
Gregory Harston <har11057@byui.edu>
2018-05-26 21:51:04 -0700

    mistake

--- web/assignments/assignment05/index.php
+++ web/assignments/assignment05/index.php
@@ -1,18 +1,11 @@
-<?php
-$dsn = "postgres://kxuikoifxgdovo:42f41f7ac4cd79d38149a6ae07314e5c3f723db50b6ad41952f86ed28d3e5126@ec2-23-23-130-158.compute-1.amazonaws.com:5432/d3eunrpj9mi32";
-$db = new PDO($dsn);
-?>
 <!DOCTYPE html>
 
 <html>
-<?php 
-require '../assingment03/assign03head.php';
-require '../assignment03/assign03header.php';
-?>
+
 <body class="light-background">
 
 	<div class="container"> 
-
+		<h1> Hello </h1>
 	</div>
 	
 	<div class="container">
