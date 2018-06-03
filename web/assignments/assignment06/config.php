98f7a5deba477cef4e1afd1a7e63a074d657aa06
Gregory Harston <har11057@byui.edu>
2018-06-02 21:02:18 -0700

    config fixes

--- web/assignments/assignment06/config.php
+++ web/assignments/assignment06/config.php
@@ -9,8 +9,14 @@ server with default setting (user 'root' with no password) */
 	$dbUser = $dbopts["user"];
 	$dbPassword = $dbopts["pass"];
 	$dbName = ltrim($dbopts["path"],'/');
-		
-	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-		
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
+	try{
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e){
+		die("ERROR: Could not connect . " . $e->getMessage());
+	}
+?>	
+	
+
+
+
