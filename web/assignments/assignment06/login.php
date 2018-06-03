6092b5c435b675aaac559c24d143fca0ef9cf282
Gregory Harston <har11057@byui.edu>
2018-06-02 22:00:16 -0700

    test

--- web/assignments/assignment06/login.php
+++ web/assignments/assignment06/login.php
@@ -27,7 +27,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
 		// $str = pg_escape_string($username);
-        $sql = "SELECT username, password FROM users WHERE username = {$str}";
+        $sql = "SELECT username, password FROM users WHERE username = {$username}";
         
         if($stmt = $pdo->prepare($sql)){
             // Bind variables to the prepared statement as parameters
@@ -59,7 +59,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     $username_err = 'No account found with that username.';
                 }
             } else{
-                echo "Oops! Something went wrong. Please try again later.";
+                echo "<h1>Oops! Something went wrong. Please try again later.</h1>";
             }
         }
         
